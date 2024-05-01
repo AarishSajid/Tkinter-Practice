3674bb66aced2b0037c08309ca718839ff449c26
AarishSajid <161928829+AarishSajid@users.noreply.github.com>
2024-05-01 15:50:18 +0500

    Add files via upload

--- entry boxes.py
+++ entry boxes.py
@@ -10,22 +10,31 @@ def clear() :
 screen = Tk()
 
 screen.title("ENTRY BOX PRACTICE")
+screen.config(background="black")
 
 t_label = Label(screen,
                 text="ENTER YOUR NAME",
-                font=("times new roman",35))
+                font=("times new roman",35),
+                bg="black",
+                fg="#FF00FF")
 
 entry_b = Entry(screen,
-                font=('times new roman',30)
+                font=('times new roman',30),
+                bg="black",
+                fg="#FF00FF"
                 )
 
 submit_btn = Button(screen,
                     text="SUBMIT",
-                    command=submit)
+                    command=submit,
+                    bg="black",
+                    fg="#FF00FF")
+
 clear_btn = Button(screen,
                     text="CLEAR",
-                    command=clear)
-
+                    command=clear,
+                    bg="black",
+                    fg="#FF00FF")
 
 
 t_label.pack()
